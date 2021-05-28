36e8f412392fe963b0a1a1724d51147f8c5eb3ed
Aleksey Golovenchik <59099639+AlGol86@users.noreply.github.com>
2021-05-29 01:23:22 +0300

    Update main.c

--- main.c
+++ main.c
@@ -12,21 +12,36 @@ int main( void )
 {
  supply ();
  initGY_521();
+ initGY_521();
  init_LED();
  out(init);
  init_tim1();
  blink(ride);
- while(1){
-  int x=get_average_GY_521(AXEL_X)/10;
-  printValue((int long)x, 0); 
+ int y=get_average_GY_521(AXEL_Y);
+ if(y<7000 && y>-7000) {
+   statement.display=on;
+ } else {
+   statement.display=off; 
+   sendCMD(CMD_DISP_OFF);
+ }
  
-  if(check_condition_GY_521(500,100,x)) {  
+ while(1){
+  int val=0;
+  val=get_average_GY_521(AXEL_X)/10;
+  if(statement.display){
+  printValue(val, 0); 
+  }
+  if(check_condition_GY_521(300,20,val)) {  
    blink(stop);
    cut_from_scedule(stop_off_task);
-   scedule(stop_off_task,0,300,0);
+   scedule(stop_off_task,0,5000,0);
   }
  }
 }
+
+
+
+
 
 
 
