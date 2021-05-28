bc7aca1c65b2a687227b21ff9dda84884f95d13e
Aleksey Golovenchik <59099639+AlGol86@users.noreply.github.com>
2021-05-29 01:22:21 +0300

    Update out.c

--- out.c
+++ out.c
@@ -16,15 +16,17 @@ extern statement_t statement;
      case stop:
          cut_from_scedule(on_light);
          cut_from_scedule(off_light);
-	 scedule(on_light, 0, 1, 7);
-         scedule(off_light, 0, 5, 7);
+         cut_from_scedule(half_on_light);
+         cut_from_scedule(half_off_light);
+	 scedule(on_light, 0, 1, 150);
+         scedule(off_light, 0, 50, 150);
          break;
     
      case ride:
          cut_from_scedule(on_light);
          cut_from_scedule(off_light);
-	 scedule(on_light, 0, 1, 50);
-         scedule(off_light, 0, 10, 50);
+	 scedule(half_on_light, 0, 1, 700);
+         scedule(half_off_light, 0, 100, 700);
          break;
   
   
@@ -40,22 +42,37 @@ extern statement_t statement;
 
 void out(out_t out){
   switch(out){
-  case init:{
+  case init:
 	PORT_out_CR2&=~BIT_out;
-	PORT_out_CR1&=~BIT_out;
-	PORT_out_ODR|=BIT_out;
+	PORT_out_CR1|=BIT_out;
+	PORT_out_ODR&=~BIT_out;
 	PORT_out_DDR|=BIT_out;
 	sysDelay(200);
         break;
-  }
-  case on:{
-  PORT_out_ODR&=~BIT_out;
-  break;
-  }
-  case off:{
+  
+  case on:
   PORT_out_ODR|=BIT_out;
   break;
-  }
+  
+  case off:
+  PORT_out_ODR&=~BIT_out;
+  break;
+  
+  case half_on:
+         cut_from_scedule(on_light);
+         cut_from_scedule(off_light);
+	 scedule(on_light, 0, 1, 6);
+         scedule(off_light, 0, 2, 6);
+  break;
+  
+  case half_off:
+         cut_from_scedule(on_light);
+         cut_from_scedule(off_light);
+         PORT_out_ODR&=~BIT_out;
+  break;
+  
  }
 }
+  
+
   
