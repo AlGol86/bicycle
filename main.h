48e90453666615a076a278c4a40c0acabddbcd54
Aleksey Golovenchik <59099639+AlGol86@users.noreply.github.com>
2021-05-29 01:22:38 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,21 +1,22 @@
 #include "iostm8s103f3.h"
-#define PORT_out_ODR       PB_ODR
-#define PORT_out_DDR       PB_DDR
-#define PORT_out_IDR       PB_IDR
-#define PORT_out_CR1       PB_CR1
-#define PORT_out_CR2       PB_CR2
-#define PIN_out   4
+#define PORT_out_ODR       PA_ODR
+#define PORT_out_DDR       PA_DDR
+#define PORT_out_IDR       PA_IDR
+#define PORT_out_CR1       PA_CR1
+#define PORT_out_CR2       PA_CR2
+#define PIN_out   3
 #define BIT_out  (1<<PIN_out)
 
 #ifndef MAIN_H
 #define MAIN_H
 
-typedef enum{init,on,off,ride,stop} out_t;
+typedef enum{off,init,on,half_on,half_off,ride,stop} out_t;
 
 
 typedef struct{
   out_t blink;
   char cond_f;
+  out_t display;
 } statement_t;
 
 void out(out_t out);
