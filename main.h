b9b1afc5ba2afba5d860ecb77eae35b1e61d0c8c
Aleksey Golovenchik <59099639+AlGol86@users.noreply.github.com>
2021-05-29 08:45:38 +0300

    Update main.h

--- main.h
+++ main.h
@@ -7,6 +7,17 @@
 #define PIN_out   3
 #define BIT_out  (1<<PIN_out)
 
+#define PORT_supply_ODR       PC_ODR
+#define PORT_supply_DDR       PC_DDR
+#define PORT_supply_IDR       PC_IDR
+#define PORT_supply_CR1       PC_CR1
+#define PORT_supply_CR2       PC_CR2
+#define pin_plus         3
+#define bit_plus                           (1<<pin_plus)
+#define pin_minus        4
+#define bit_minus                          (1<<pin_minus)
+#define bit_mask_plus_minus  (bit_plus | bit_minus)
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -22,4 +33,5 @@ typedef struct{
 void out(out_t out);
 void blink(out_t blink);
 void stop_light(void);
+void supply (out_t mode);
 #endif
